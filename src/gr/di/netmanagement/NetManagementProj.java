6ff9c9212eae35cba6220bd2ac327f68348651cb
maenolis <maenolis@hpProbook>
2015-07-09 22:07:41 +0300

    Git Tests.

--- src/gr/di/netmanagement/NetManagementProj.java
+++ src/gr/di/netmanagement/NetManagementProj.java
@@ -4,8 +4,21 @@ import java.io.File;
 
 public class NetManagementProj {
 
+	private static File baseStationFile;
+
+	private static File batteryFile;
+
+	private static File gpsFile;
+
+	private static File wifiFile;
+
 	public static void main(String[] args) {
 
 		System.out.println((new File("data/base_station.csv")).exists());
+
+		baseStationFile = new File("data/base_station.csv");
+		batteryFile = new File("data/battery.csv");
+		gpsFile = new File("data/gps.csv");
+		wifiFile = new File("data/wifi.csv");
 	}
 }
