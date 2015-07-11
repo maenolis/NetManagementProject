b04305116252e5bca7347b6f1a1e9373e53123f8
Manolis Varvarigos <psimukas@hotmail.com>
2015-07-11 15:39:57 +0300

    rania test

--- src/gr/di/netmanagement/NetManagementProj.java
+++ src/gr/di/netmanagement/NetManagementProj.java
@@ -1,6 +1,9 @@
 package gr.di.netmanagement;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class NetManagementProj {
 
@@ -18,8 +21,31 @@ public class NetManagementProj {
 		batteryFile = new File("data/battery.csv");
 		gpsFile = new File("data/gps.csv");
 		wifiFile = new File("data/wifi.csv");
-		System.out.println("test");
-		
-		System.out.println("test3");
+
+		FileReader fr = null;
+		BufferedReader br = null;
+		String line = null;
+		try {
+			fr = new FileReader(baseStationFile);
+			br = new BufferedReader(fr, 1024);
+			while (true) {
+				line = br.readLine();
+				if (line == null) {
+					break;
+				}
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
