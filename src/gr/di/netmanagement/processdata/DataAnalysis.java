738de78108b875f88fa99296c68449b675edc23d
Manolis Varvarigos <mvarvarigos@cognity.gr>
2015-08-24 15:12:10 +0300

    Cleanup.

--- src/gr/di/netmanagement/processdata/DataAnalysis.java
+++ src/gr/di/netmanagement/processdata/DataAnalysis.java
@@ -7,8 +7,6 @@ import gr.di.netmanagement.beans.PointOfInterest;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
 /**
  * The Class DataAnalysis.
@@ -17,9 +15,6 @@ public class DataAnalysis {
 
 	/**
 	 * Instantiates a new data analysis.
-	 *
-	 * @param numOfStayPoints
-	 *            the num of stay points
 	 */
 	public DataAnalysis() {
 
@@ -36,8 +31,8 @@ public class DataAnalysis {
 	 *            the user
 	 * @param dataProcessor
 	 *            the data processor
-	 * @param dMin
-	 *            the d min
+	 * @param tMin
+	 *            the t min
 	 * @param dMax
 	 *            the d max
 	 * @param tMax
@@ -49,12 +44,10 @@ public class DataAnalysis {
 	public static ArrayList<PointOfInterest> analizeLocations(final Date from,
 			final Date to, final String user,
 			final DataProcessor dataProcessor, final float tMin,
-			final float dMax, final int tMax, final int maxMeters) {
+			final float dMax, final int tMax) {
 
 		ArrayList<Object> list = discardFromDateRange(from, to, user,
 				dataProcessor);
-
-		list = discardFromTimeAndSpaceDistance(list, tMax, maxMeters);
 
 		ArrayList<PointOfInterest> retList = new ArrayList<PointOfInterest>();
 
@@ -91,6 +84,7 @@ public class DataAnalysis {
 								calculateCentroid(tmpList),
 								((LocationBean) list.get(i)).getTimestamp(),
 								((LocationBean) list.get(j - 1)).getTimestamp());
+						tmpList.clear();
 						retList.add(poi);
 					}
 					i = j;
@@ -128,6 +122,7 @@ public class DataAnalysis {
 
 		/* Get wifi and baseStation lists for given user. */
 		final ArrayList<Object> wifiList = dataProcessor.getWifiMap().get(user);
+		System.out.println(wifiList);
 		final ArrayList<Object> baseStationList = dataProcessor
 				.getBaseStationMap().get(user);
 		/* Gather wifi locations. */
@@ -150,93 +145,12 @@ public class DataAnalysis {
 	}
 
 	/**
-	 * Discard from time distance.
-	 *
-	 * @param list
-	 *            the list
-	 * @param tMax
-	 *            the t max
-	 * @param maxMeters
-	 *            the max meters
-	 * @return the array list
-	 */
-	public static ArrayList<Object> discardFromTimeAndSpaceDistance(
-			final ArrayList<Object> list, final int tMax, final int maxMeters) {
-
-		/* Lists with less than 3 beans check. */
-		if (list.size() == 1) {
-			return list;
-		} else if (list.size() == 2) {
-			final Bean bean1 = (Bean) list.get(0);
-			final Bean bean2 = (Bean) list.get(1);
-			if (timeDistanceBoolean(bean1.getTimestamp(), bean2.getTimestamp(),
-					tMax)) {
-				return list;
-			} else {
-				return null;
-			}
-		}
-
-		final TreeMap<Date, LocationBean> map = new TreeMap<Date, LocationBean>();
-		/* Sort beans by date. */
-		for (final Object obj : list) {
-			final LocationBean bean = (LocationBean) obj;
-			map.put(bean.getTimestamp(), bean);
-		}
-
-		/* Check beans for time distance. */
-		final Object[] dates = map.keySet().toArray();
-		final TreeSet<Date> datesSet = new TreeSet<Date>();
-		final TreeSet<Date> emptyDatesSet = new TreeSet<Date>();
-
-		for (int i = 1; i < dates.length; i++) {
-			final Date date1 = (Date) dates[i - 1];
-			final Date date2 = (Date) dates[i];
-			final Date date3 = (Date) dates[i + 1];
-			final int previousSize = datesSet.size();
-			if (timeDistanceBoolean(date1, date2, tMax)
-					&& distanceInMetersBoolean(map.get(date1).getLocation(),
-							map.get(date2).getLocation(), maxMeters)) {
-				datesSet.add(date1);
-				datesSet.add(date2);
-			}
-			if (timeDistanceBoolean(date2, date3, tMax)
-					&& distanceInMetersBoolean(map.get(date2).getLocation(),
-							map.get(date3).getLocation(), maxMeters)) {
-				datesSet.add(date2);
-				datesSet.add(date3);
-			}
-			if (previousSize == datesSet.size()) {
-				emptyDatesSet.add(date2);
-			}
-		}
-
-		for (final Date date : emptyDatesSet) {
-			datesSet.add(date);
-		}
-
-		/* Add beans to cleared list. */
-		list.clear();
-		for (final Date date : datesSet) {
-
-			if (!emptyDatesSet.contains(date)) {
-				list.add(map.get(date));
-			} else if (list.size() > 0 && (list.get(list.size() - 1) != null)) {
-				list.add(null);
-			}
-		}
-		return list;
-	}
-
-	/**
 	 * Check if time distance between 2 dates is less than tMax minutes.
 	 *
 	 * @param date1
 	 *            the date1
 	 * @param date2
 	 *            the date2
-	 * @param tMax
-	 *            the t max
 	 * @return true, if successful
 	 */
 	private static long timeDistance(final Date date1, final Date date2) {
@@ -244,40 +158,6 @@ public class DataAnalysis {
 		final long diffInMilliseconds = date1.getTime() - date2.getTime();
 		return Math.abs(diffInMilliseconds);
 	}
-
-	private static boolean timeDistanceBoolean(final Date date1,
-			final Date date2, final int tMax) {
-
-		final long diffInMilliseconds = date1.getTime() - date2.getTime();
-		return (Math.abs(diffInMilliseconds) < 60 * 1000 * tMax);
-	}
-
-	// /**
-	// * Calculate centroids.
-	// *
-	// * @param list
-	// * the list
-	// * @return the array list of centroids' locations
-	// */
-	// private static ArrayList<Location> calculateCentroids(
-	// final ArrayList<Object> list) {
-	//
-	// final ArrayList<LocationBean> currentList = new
-	// ArrayList<LocationBean>();
-	// final ArrayList<Location> centroids = new ArrayList<Location>();
-	// for (int i = 0; i < list.size(); i++) {
-	// if (list.get(i) != null) {
-	// final LocationBean locationBean = (LocationBean) list.get(i);
-	// currentList.add(locationBean);
-	// } else {
-	// if (currentList.size() > 0) {
-	// centroids.add(calculateCentroid(currentList));
-	// currentList.clear();
-	// }
-	// }
-	// }
-	// return centroids;
-	// }
 
 	/**
 	 * Calculate centroid.
@@ -305,36 +185,8 @@ public class DataAnalysis {
 	 *            the location1
 	 * @param location2
 	 *            the location2
-	 * @param metersLimit
-	 *            the meters limit
-	 * @return true, if successful
+	 * @return the double
 	 */
-	private static boolean distanceInMetersBoolean(final Location location1,
-			final Location location2, final int metersLimit) {
-
-		final double lat1 = location1.getLatitude();
-		final double lon1 = location1.getLongtitude();
-		final double lat2 = location2.getLatitude();
-		final double lon2 = location2.getLongtitude();
-
-		/* Radius of the earth. */
-		final int R = 6371;
-
-		final Double latDistance = Math.toRadians(lat2 - lat1);
-		final Double lonDistance = Math.toRadians(lon2 - lon1);
-		final Double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-				+ Math.cos(Math.toRadians(lat1))
-				* Math.cos(Math.toRadians(lat2)) * Math.sin(lonDistance / 2)
-				* Math.sin(lonDistance / 2);
-		final Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-		/* Convert to meters. */
-		double distance = R * c * 1000;
-
-		distance = Math.pow(distance, 2);
-
-		return Math.sqrt(distance) < metersLimit;
-	}
-
 	private static double distanceInMeters(final Location location1,
 			final Location location2) {
 
